439318029f85e972091a36242c2dd812683705e9
Clark Annable <supergamebase@gmail.com>
2026-08-07 11:50:56 -0400

    cleanup

--- src/videoplayer.cpp
+++ src/videoplayer.cpp
@@ -7,10 +7,10 @@ class PlayerSlideStyle : public QProxyStyle
 {
 public:
     int styleHint(
-        StyleHint         hint,
-        const QStyleOption     *option = nullptr,
-        const QWidget          *widget = nullptr,
-        QStyleHintReturn *returnData = nullptr) const override
+        StyleHint           hint,
+        const QStyleOption  *option     = nullptr,
+        const QWidget       *widget     = nullptr,
+        QStyleHintReturn    *returnData = nullptr) const override
     {
         if (hint == QStyle::SH_Slider_AbsoluteSetButtons)
             return (Qt::LeftButton | Qt::MiddleButton | Qt::RightButton);
